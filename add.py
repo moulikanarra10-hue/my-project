274c16e63d9b80b6b4d7483f9ebf071f6f6b5771
Moulika Narra <moulika.narra10@gmail.com>
2026-09-09 18:23:21 +0530

    Initial project

---
+++ add.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    result = add_numbers(20, 20)
+    print("The result is:", result)
---
+++ test_add.py
@@ -0,0 +1,5 @@
+from add import add_numbers
+
+
+def test_add_numbers():
+    assert add_numbers(20,20) == 40
